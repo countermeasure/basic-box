6750262603df9505dcfcfb818b78556f79ebc6c7
Sky <sky@skychristensen.com>
2023-07-06 23:14:42 +1000

    Upgrade Mullvad VPN package

--- get_extra_downloads.sh
+++ get_extra_downloads.sh
@@ -8,7 +8,7 @@ mkdir -p extra/downloads
 cd extra/downloads || exit 1
 
 # Ensure the Mullvad VPN app package is present.
-wget --timestamping https://mullvad.net/media/app/MullvadVPN-2022.4_amd64.deb
+wget --timestamping https://mullvad.net/media/app/MullvadVPN-2023.4_amd64.deb
 
 # Ensure the fd package is present.
 wget --timestamping "https://github.com/sharkdp/fd/releases/download/v8.4.0/\
