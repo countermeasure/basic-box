26a2e5a6770da7d34ba052aec344ed78b87c39fc
Sky <sky@skychristensen.com>
2024-07-27 16:20:39 +1000

    Tune up neovim config

--- extra/dotfiles/nvim_plugins_telescope.lua
+++ extra/dotfiles/nvim_plugins_telescope.lua
@@ -7,21 +7,31 @@ return {
     "nvim-telescope/telescope.nvim",
     keys = function()
       return {
-        { "<leader>Fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
         {
-          "<leader>Ff",
-          Util.telescope("files"),
-          desc = "Find Files (root dir)",
+          "<leader>Fb",
+          "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
+          desc = "Buffers",
         },
         {
+          "<leader>Fc",
+          LazyVim.pick.config_files(),
+          desc = "Find Config File",
+        },
+        { "<leader>Ff", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+        {
           "<leader>FF",
-          Util.telescope("files", { cwd = false }),
+          LazyVim.pick("files", { root = false }),
           desc = "Find Files (cwd)",
+        },
+        {
+          "<leader>Fg",
+          "<cmd>Telescope git_files<cr>",
+          desc = "Find Files (git-files)",
         },
         { "<leader>Fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
         {
           "<leader>FR",
-          Util.telescope("oldfiles", { cwd = vim.loop.cwd() }),
+          LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }),
           desc = "Recent (cwd)",
         },
       }
