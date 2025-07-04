cf8e6ed1e34a616d0eb8833e1444b4ef9392ffe2
Sky <sky@skychristensen.com>
2025-07-04 22:06:27 +1000

    Add Flameshot

--- extra/box.sh
+++ extra/box.sh
@@ -1147,6 +1147,14 @@ bluetooth-quick-connect@bjarosze.gmail.com/schemas"
   _test_gsettings gnome.desktop.interface cursor-blink-timeout 86400
   _test_package_is_installed rsync
   _test_file_exists "${user_dir}"/.box/build.txt
+  _test_package_is_installed flameshot
+  _test_file_exists "${user_config_dir}"/flameshot/flameshot.ini
+  custom_keybinding_4="${custom_keybinding_setting}:\
+${custom_keybindings_key_path}/custom4/"
+  _test_gsettings ${custom_keybinding_4} binding "'Print'"
+  _test_gsettings ${custom_keybinding_4} command "'flameshot gui'"
+  _test_gsettings ${custom_keybinding_4} name "'Screenshot'"
+  _test_gsettings ${keybindings_setting} show-screenshot-ui '@as []'
   # Tests arising from first_boot.sh
   _test_command_output 'sudo ufw status' 'Status: active'
   _test_command_output 'mullvad auto-connect get' 'Autoconnect: on'
