c055756c09e388d2a635bab511756b85a042182b
Sky <sky@skychristensen.com>
2025-07-14 11:41:45 +1000

    Create an installation file when installing

--- extra/box.sh
+++ extra/box.sh
@@ -1141,6 +1141,7 @@ ${custom_keybindings_key_path}/custom4/"
   _test_gsettings ${custom_keybinding_4} name "'Screenshot'"
   _test_gsettings ${keybindings_setting} show-screenshot-ui '@as []'
   _test_directory_exists "${user_dir}"/Data
+  _test_directory_exists "${user_dir}"/.box/installation.txt
   # Tests arising from first_boot.sh
   _test_command_output 'sudo ufw status' 'Status: active'
   _test_command_output 'mullvad auto-connect get' 'Autoconnect: on'
